82ec601b4cf383473d2d54c381512ab461c3721d
Aleff Fernandes <aleff.fernandes236@gmail.com>
2022-10-18 20:41:14 -0300

    Atualização do aumento salarial

--- PYTHON - USO ADMINISTRATIVO/Aumento Salarial/Calcular aumento %.py
+++ PYTHON - USO ADMINISTRATIVO/Aumento Salarial/Calcular aumento %.py
@@ -1,6 +1,6 @@
-#Aumentar em 15% o salário do funcionário
 salarioAtual = float(input('Informe o salário atual do funcionário: '))
-aumento = salarioAtual * 15 / 100
+aumento = float(input('Informe, em porcentagem, quanto será o aumento: '))
+valorAumento = salarioAtual * aumento / 100
 salarioReajustado = salarioAtual + aumento
 
-print ('O salário de R${:.2f} do funcionário, ao aumentar em 15%, no valor de R${:.2f}, irá passar para R${:.2f}'.format(salarioAtual, aumento, salarioReajustado))
+print ('O salário de R${:.2f} do funcionário, ao aumentar em {:.2f}%, no valor de R${:.2f}, irá passar para R${:.2f}'.format(salarioAtual, aumento, valorAumento, salarioReajustado))
